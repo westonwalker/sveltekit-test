dcd68c0c3a152e2090b76e6a5b50b1773e78729a
Weston Walker <walk8919@gmail.com>
2022-12-27 14:24:09 -0600

    added title tags.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/about">About</a>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
 <h1>About this site</h1>
 <p>TODO...</p>
 <a href="/">Home</a>
